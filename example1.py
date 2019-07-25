ec865a772e2e4f72735b10ab7bdc1b75b3384339
Oktay Yuceel <okty.352.503@gmail.com>
2019-07-26 01:56:02 +0300

    version 1

---
+++ example1.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 25 01:00:32 2019
+
+@author: oktay
+"""
+
+import turtle
+
+loadWindow = turtle.Screen() 
+kalem=turtle.Pen()
+kalem.width(10)
+kalem.speed(1)
+kalem.shape("turtle")
+
+kalem.color("red")
+kalem.penup()
+kalem.goto(-100,0)
+kalem.pendown()
+kalem.left(90)
+kalem.forward(100)
+kalem.right(315)
+kalem.backward(141)
+kalem.right(45)
+kalem.forward(100)
+
+kalem.penup()
+kalem.goto(20,100)
+kalem.pendown()
+kalem.color("purple")
+
+kalem.backward(100)
+kalem.right(90)
+kalem.forward(100)
+kalem.left(90)
+kalem.forward(100)
+
+kalem.penup()
+kalem.goto(140,0)
+kalem.pendown()
+kalem.color("pink")
+
+kalem.forward(100)
+kalem.right(90)
+kalem.forward(60)
+kalem.right(90)
+kalem.forward(60)
+kalem.right(90)
+kalem.forward(50)
+kalem.left(315)
+kalem.backward(60)
+import turtle
+turtle.exitonclick() 
---
+++ example2.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 25 02:53:40 2019
+
+@author: oktay
+"""
+import turtle 
+colors = ['red', 'purple', 'blue', 'green', 'orange', 'yellow'] 
+pen = turtle.Pen() 
+turtle.bgcolor('black') 
+for x in range(360): 
+    pen.pencolor(colors[x%6]) 
+    pen.width(x/100 + 1) 
+    pen.forward(x)   #Moves the turtle forward by the specified amount
+    pen.circle(5*x)  
+    pen.right(60)    #Turns the turtle clockwise
+
